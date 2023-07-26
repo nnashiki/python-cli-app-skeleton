c89f5db9aeb87b71ceadafec5cd79b101d36e381
Niten Nashiki <niten.nashiki@lightz-inc.com>
2023-07-26 18:34:28 +0900

    add log

--- nassypy/cli.py
+++ nassypy/cli.py
@@ -1,4 +1,8 @@
 import click
+import logging
+LOG_LEVEL = ["DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"]
+
+logger = logging.getLogger(__name__)
 
 
 @click.group()
@@ -11,6 +15,24 @@ def cli(ctx, main_param_bool, main_param_str, main_param_int):
     ctx.obj['MAIN_PARAM_BOOL'] = main_param_bool
     ctx.obj['MAIN_PARAM_STR'] = main_param_str
     ctx.obj['MAIN_PARAM_INT'] = main_param_int
+
+
+    # parse log level
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level")
+
+    # この モジュールの ログレベルを設定
+    logger.setLevel(numeric_level)
+
+    # 標準出力(コンソール)にログを出力するハンドラを生成する
+    stream_h = logging.StreamHandler(sys.stdout)
+    fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%dT%H:%M:%S")
+    stream_h.setFormatter(fmt)
+    # ハンドラをロガーに紐づける
+    logger.addHandler(stream_h)
+
+    logger.debug(f"{args=}")
 
 
 @cli.command()
